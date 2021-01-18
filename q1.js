4f389acaed1caa2e39f379ad36bcce23b11606cc
ofir-danan <77383793+ofir-danan@users.noreply.github.com>
2021-01-18 13:35:59 +0200

    q1

---
+++ q1.js
@@ -0,0 +1,9 @@
+function question1() { 
+    let num1 = prompt("first number");
+    let num2 = prompt("second number");
+    if (num1 > num2) {
+        alert(num1);
+    } else {
+        alert(num2 + " is the bigger number");
+    }
+}
